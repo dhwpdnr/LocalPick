cf5450fa0cc288c28589659d40b07512478eb414
dhwpdnr <ink1237@gmail.com>
2022-10-27 11:30:41 +0900

    search api querystring

--- search/apis.py
+++ search/apis.py
@@ -10,21 +10,19 @@ class SearchAPI(generics.ListAPIView):
     # permission_classes = (IsAuthenticated,)
     serializer_class = StoreSerializer
 
-    def get_queryset(self, search):
+    def get_queryset(self):
         # instance 는 우리가 사용할 쿼리 / url 에서 값 받아서 조회도 가능
-        instance = Store.objects.filter(
-                Q(store_name__icontains=search) |
-                Q(store_adress__icontains=search))
+        instance = Store.objects.all()
         return instance
 
-    # query string으로 처리
     def get(self, request, *args, **kwargs):
         search_word = request.GET.get("s", "")
         # self.get_queryset() 부분은 사전에 정의 해둔 queryset 가져온
-        print(search_word)
-        queryset = self.filter_queryset(self.get_queryset(search_word))
+        query = self.filter_queryset(self.get_queryset())
+        queryset = query.filter(
+            Q(store_name__icontains=search_word) |
+            Q(store_adress__icontains=search_word))
         serializer = self.get_serializer(queryset, many=True)
-        print(serializer.data)
         return Response(serializer.data)
 
     # url에 검색어 직접 넣어서 넘겨줌
