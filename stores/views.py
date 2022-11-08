ae65efc4681de03bfa03ad4874d0afae723ee8b0
dhwpdnr <ink1237@gmail.com>
2022-11-08 09:53:21 +0000

    merge

--- stores/views.py
+++ stores/views.py
@@ -57,8 +57,8 @@ class StoreListView(APIView):
             return redirect("users:login")
         user_id = request.session.get("_auth_user_id", None)
         taste = Appetite.objects.filter(user_id=user_id).first()
-        if taste is None:
-            return render(request, "users/taste.html")
+        # if taste is None:
+        #     return render(request, "users/taste.html")
         # {{ user.정보 }}로 사용 ex) {{ user.nickname }}
         return render(request, "store/list.html", info)
 
